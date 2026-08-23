e35a5edc9939e4b47acdc1004897394a0a26b1ae
avdeevgreyfog-cmd <avdeevgreyfog@gmail.com>
2026-08-23 14:29:19 +0300

    Remove superseded heightfield test runner

--- tests/heightfield-runner.mjs
+++
@@ -1,9 +0,0 @@
-import {spawnSync} from "node:child_process";
-
-const result=spawnSync(
-  process.execPath,
-  ["--experimental-strip-types","--experimental-specifier-resolution=node","--test","tests/heightfield.test.ts"],
-  {stdio:"inherit",env:process.env},
-);
-
-process.exit(result.status??1);
